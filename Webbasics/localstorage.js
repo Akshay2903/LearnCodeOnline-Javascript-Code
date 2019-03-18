5d87731b5f3c61683ae7b410a7bf075fabe81add
akshaypal2903@gmail.com <akshaypal2903@gmail.com>
2019-03-18 13:54:45 +0530

    Javascript code for beginner

---
+++ Webbasics/localstorage.js
@@ -0,0 +1,17 @@
+localStorage.setItem('hero', 'thor');
+
+localStorage.setItem('todo', 'Buy Ice tea');
+var myHero = localStorage.getItem('hero');
+console.log(myHero);
+
+console.log(localStorage.getItem('todo'));
+
+localStorage.setItem('todo', 'Record Youtube videos');
+console.log(localStorage.getItem('todo'));
+
+// localStorage.removeItem('hero');
+
+localStorage.clear()
+myHero = localStorage.getItem('hero');
+console.log(myHero);
+
